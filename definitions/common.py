690728879728ede2f275c6dabbd7f645a0f555ff
Ji <Phan.Huy.Cong@gmail.com>
2025-04-03 01:18:19 +0700

    Create definitions

---
+++ definitions/common.py
@@ -0,0 +1,15 @@
+SUBSCRIPTION_CANCELED_STRINGS = ["Subscription charge canceled",\
+                                 "Subscription charge frozen"]
+
+SUBSCRIPTION_ACTIVATED_STRINGS = ["Subscription charge activated",\
+                                  "Subscription charge unfrozen"]
+
+ONE_TIME_ACTIVATED_STRINGS = ["Charge activated"]
+
+INSTALLED_STRING = "Installed"
+UNINSTALLED_STRING = "Uninstalled"
+
+KEY_FIELD = "Shop domain"
+EVENT_FIELD = "Event"
+DETAILS_FIELD = "Details"
+TIME_FIELD = "Date"
